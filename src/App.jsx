1c3ed21ef4e54f6d04d728e7a79e0798caca379b
Pablo <pablo.j98@gmail.com>
2022-11-15 00:49:11 -0400

    mobile view fixed

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,7 @@ function App() {
 
   Data.forEach( (data,key) => {
     key=data.name; 
-    console.log(data.testimony.indexOf('freeCodeCamp'.toLowerCase()))
-    testimonies.push(<Testimony {...data} />);
+    testimonies.push(<Testimony key={key} {...data} />);
   });
 
   return (
@@ -28,22 +27,3 @@ function App() {
 export default App;
 
 
-/*
-        <Testimony {...Data[2]} />
-
-<Testimony 
-          image = 'sarah'
-          name = 'Sarah Chima'
-          country = 'Nigeria'
-          position = 'Software Engineer'
-          brand = 'ChatDesk'
-          testimony = "freeCodeCamp was the gateway to my career as a software developer. The well-structured curriculum took my coding knowledge from a total beginner level to a very confident level. It was everything I needed to land my first dev job at an amazing company." />
-        <Testimony 
-          image = 'emma'
-          name = 'Emma Bostian'
-          country = 'Sweden'
-          position = 'Software Engineer'
-          brand = 'Spotify'
-          testimony = "I've always struggled with learning JavaScript. I've taken many courses but freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer at Spotify." />
-
-*/
